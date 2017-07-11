584f844c0e5977eef3bea23366a78efd7a971bb8
Joshua Copeland <Josh@EngagedNation.com>
2017-07-10 20:38:12 -0700

    fixed dir

--- code/example/web/watchers.php
+++ code/example/web/watchers.php
@@ -1,6 +1,6 @@
 <?php
 
-require getcwd() . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'include.php';
+require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'include.php';
 
 // Come back here when debugger is in thatFunction to see $var is relevant in the scope you are "watching" it in
 $var = 'Call Stack Example';
